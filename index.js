fd9f2f398a0c06f576d210b9ec9e1a4c8742ac9b
Sophie Ryrberg <99020813+sophieryrberg@users.noreply.github.com>
2024-05-06 22:52:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ const CANVAS_HEIGHT = 600;
  *
  * @p5jsMethod
  ****************************************************/
-function preload() {}
+function preload() {
+  backgroundImg = loadImage("images/background.png");
+  unicornImg = loadImage("images/unicorn.png");
+  boidImg = loadImage("images/boid.png");
+}
 
 /****************************************************
  * setup() runs only once and is used to initialize "stuffs"
@@ -57,7 +61,11 @@ function preload() {}
  *
  * @p5jsMethod
  ****************************************************/
-function setup() {}
+function setup() {
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  background = new Background();
+  unicorn = new Unicorn();
+}
 
 /****************************************************
  * draw() is a magical p5 function which runs repeatedly and is used to draw graphics/stuffs
@@ -65,14 +73,46 @@ function setup() {}
  *
  * @p5jsMethod
  ****************************************************/
-function draw() {}
+function draw() {
+  background.show();
+  background.update();
+  unicorn.show();
+  unicorn.update();
+
+  if (frameCount % 100 === 0) {
+    pipes.push(new Pipe());
+  }
+
+  for (let pipe of pipes) {
+    pipe.show();
+    pipe.update();
+    if (pipe.hits(unicorn)) gameOver();
+    if (pipe.pass(unicorn)) score++;
+  }
+  showScore();
+
+  if (frameCount % 75 === 0) boids.push(new Boid());
+  for (let boid of boids) {
+    boid.show();
+    boid.update();
+    if (boid.hits(unicorn)) {
+      score += 3;
+      boids.splice(boids.indexOf(boid), 1);
+    }
+  }
+}
 
 /****************************************************
  * Set the space button to make the unicorn fly
  *
  * @p5jsMethod
  ****************************************************/
-function keyPressed() {}
+function keyPressed() {
+  if (key === " ") {
+    unicorn.up();
+    if (isOver) startGame();
+  }
+}
 
 /****************************************************
  * Create a new unicorn and new pipe
@@ -81,7 +121,15 @@ function keyPressed() {}
  *
  * @customMethod
  ****************************************************/
-function startGame() {}
+function startGame() {
+  background = new Background(); // Reset the background's x position.
+  unicorn = new Unicorn(); // we create a new unicorn to original position.
+  pipes = []; // We will need an empty pipes array to reset pipe positions.
+  boids = []; // We will need an empty boids array. We will implement later.
+  isOver = false; // Set isOver to false when starting the game again.
+  loop(); // Start looping again (adding frames), else game will be paused.
+  score = 0; // We set the score to 0. We will implement this in the next step!
+}
 
 /****************************************************
  * Display text on screen, using text function from p5
@@ -89,7 +137,13 @@ function startGame() {}
  *
  * @customMethod
  ****************************************************/
-function gameOver() {}
+function gameOver() {
+  textSize(50);
+  fill(0o0);
+  text("GAME OVER", 50, 300);
+  isOver = true;
+  noLoop();
+}
 
 /****************************************************
  * Display text on screen, using text function from p5
@@ -97,4 +151,8 @@ function gameOver() {}
  *
  * @customMethod
  ****************************************************/
-function showScore() {}
+function showScore() {
+  fill(0o0);
+  textSize(32);
+  text("Score: " + score, 1, 32);
+}
