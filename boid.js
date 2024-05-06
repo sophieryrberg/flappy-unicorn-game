f2f7b70fad51ed9930404c430d8101767c052505
Sophie Ryrberg <99020813+sophieryrberg@users.noreply.github.com>
2024-05-06 22:52:12 +0200

    Update boid.js

--- boid.js
+++ boid.js
@@ -15,17 +15,37 @@ class Boid {
    * together with x, y, width and height
    * @custom
    ****************************************************/
-  show() {}
+  show() {
+    image(boidImg, this.x, this.y, this.width, this.height);
+  }
 
   /****************************************************
    * Make the boid start outside of the canvas to the right and
    * move to the left.
    * Use x and speed.
    ****************************************************/
-  update() {}
+  update() {
+    this.x -= this.speed;
+  }
 
   /****************************************************
    * Here you want the unicorn to hit the boid
    ****************************************************/
-  hits(unicorn) {}
+  hits(unicorn) {
+    if (
+      this.y < unicorn.y + unicorn.height &&
+      this.y + this.height > unicorn.y
+    ) {
+      if (
+        this.x < unicorn.x + unicorn.width &&
+        this.x + this.width > unicorn.x
+      ) {
+        if (!this.taken) {
+          this.taken = true;
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
